e44a1b0547863d6f521f447a931a40c21c501da9
Maciej Michalec <maciej.michalec@gmail.com>
2019-07-22 22:05:46 +0200

    Added starting terminal by Alt+F2

--- polydev-wm.c
+++ polydev-wm.c
@@ -7,12 +7,31 @@
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 FILE *logfile;
+char terminalApp[64];
+
+void readConfiguration() 
+{
+    char key[64];
+	char value[64];
+
+ 	FILE* config = fopen("/etc/polydevWM.conf","r");
+	while (fscanf(config, "%s %s\n", &key[0], &value[0]) != EOF)
+	{
+		if (strcmp(key, "terminal-app") == 0)
+        {
+            strcpy(terminalApp, value);
+        }
+	}
+    fclose(config);
+}
 
 int main()
 {
+    readConfiguration();
     logfile = fopen("/tmp/polydevWM.log", "a+");
     Display * dpy;
     Window root;
@@ -21,14 +40,16 @@ int main()
     XEvent ev;
     XEvent msg; // msg to send (alt+f4)
 
-    if(!(dpy = XOpenDisplay(0x0))) return 1;
+    if (!(dpy = XOpenDisplay(0x0))) return 1;
 
     root = DefaultRootWindow(dpy);
 
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F4")), Mod1Mask, root,
-             True, GrabModeAsync, GrabModeAsync);
     XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask, root,
             True, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F2")), Mod1Mask, root,
+             True, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F4")), Mod1Mask, root,
+             True, GrabModeAsync, GrabModeAsync);
     XGrabButton(dpy, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
             GrabModeAsync, None, None);
     XGrabButton(dpy, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
@@ -38,7 +59,7 @@ int main()
     {
         XNextEvent(dpy, &ev);
         // fprintf(logfile, "Event captured, type=%d\n", ev.type);
-        if(ev.type == KeyPress && ev.xkey.subwindow != None)
+        if (ev.type == KeyPress && ev.xkey.subwindow != None)
         {
             if (ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("F4")))
             {
@@ -51,12 +72,16 @@ int main()
                 msg.xclient.data.l[1] = CurrentTime;
                 XSendEvent(dpy, ev.xkey.subwindow, False, 0, &msg);
             }
-            else
+            else if (ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("F1")))
             {
                 XRaiseWindow(dpy, ev.xkey.subwindow);
             }
         }
-        else if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
+        else if (ev.type == KeyPress && ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("F2")))
+        {
+            system(terminalApp);
+        }
+        else if (ev.type == ButtonPress && ev.xbutton.subwindow != None)
         {
             XGrabPointer(dpy, ev.xbutton.subwindow, True,
             PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
@@ -64,10 +89,10 @@ int main()
             XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
             start = ev.xbutton;
         }
-        else if(ev.type == MotionNotify)
+        else if (ev.type == MotionNotify)
         {
             int xdiff, ydiff;
-            while(XCheckTypedEvent(dpy, MotionNotify, &ev));
+            while (XCheckTypedEvent(dpy, MotionNotify, &ev));
             xdiff = ev.xbutton.x_root - start.x_root;
             ydiff = ev.xbutton.y_root - start.y_root;
             XMoveResizeWindow(dpy, ev.xmotion.window,
@@ -76,7 +101,7 @@ int main()
                 MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
                 MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
         }
-        else if(ev.type == ButtonRelease)
+        else if (ev.type == ButtonRelease)
         {
             XUngrabPointer(dpy, CurrentTime);
         }
